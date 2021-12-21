98e756ff06ffcc6c7addd062195e9b55964261e9
Rivin-Roy <96478521+Rivin-Roy@users.noreply.github.com>
2021-12-21 18:26:06 +0530

    Create arithmetic_operators.py

---
+++ arithmetic_operators.py
@@ -0,0 +1,13 @@
+#Arithmetic Operators - # for single line comment
+a,b,c,d,e=10,5,2,3.5,"Apple"
+"""multi line comment"""
+print(a)
+print(b)
+print(c)
+print("sum=" ,2+3)
+print("Difference=" ,a-b)
+print("Product=" ,a*b)
+print("Quotient=" ,b/c)
+print("Floor Division=" ,b//c)
+print("Modulus=" ,b%c)
+print("Exponent=" ,b**2)
